d68a2768ced4153c362913fe04eaaa15e8cbd815
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-02-25 17:05:10 +0300

    Add create board button

---
+++ src/components/MainPage/components/CreateButton.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+import './CreateButton.scss';
+
+
+const CreateButton = () => {
+  return (
+    <div className='create-button'>
+      <h3>Create a new board...</h3>
+    </div>
+  );
+};
+
+export default CreateButton;
