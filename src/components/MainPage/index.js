758a376686fe4b861ecd397e39d4fc1d48fdbacb
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-02-25 15:55:44 +0300

    Add route for main page

---
+++ src/components/MainPage/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+import './index.scss';
+
+
+const MainPage = () => {
+  return (
+    <div className='main-page'>
+      Hello from Main Page! :)
+    </div>
+  );
+};
+
+export default MainPage;
