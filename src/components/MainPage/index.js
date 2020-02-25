2339d299ef2eef8a125b356f8d08c0fe0f00b5b9
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-02-25 16:29:41 +0300

    Add hedaer in the Main page

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -2,11 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 import './index.scss';
 
+import logo from './icons/logo.svg';
+
 
 const MainPage = () => {
+  const [mouseOverOnLogo, setMouseOverOnLogo] = useState(false);
+
   return (
     <div className='main-page'>
-      Hello from Main Page! :)
+      <div className='main-page__header'>
+        <img
+          src={logo}
+          alt='logo'
+          className={`main-page__logo ${mouseOverOnLogo ? 'main-page__logo_hover' : ''}`}
+          onMouseOver={() => setMouseOverOnLogo(true)}
+          onMouseLeave={()=> setMouseOverOnLogo(false)}
+        />
+      </div>
     </div>
   );
 };
