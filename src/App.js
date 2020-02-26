f3e192e68657fb6f53b61d9a7c39607965083cef
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-02-26 14:32:38 +0300

    Change Main page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
+import './App.scss'
+
+import logo from './components/MainPage/icons/logo.svg';
 import MainPage from '../src/components/MainPage';
+import CurrentBoard from '../src/components/CurrentBoard';
 
 
 const App = () => {
+  const [mouseOverOnLogo, setMouseOverOnLogo] = useState(false);
+
   return (
-    <Router>
-    <div className="router">
-      <Route exact path="/" component={MainPage}/>
-    </div>
-  </Router>
+    <Fragment>
+      <Router>
+        <div className="router">
+          <div className="router__header">
+            <Link to={'/'}>
+              <img
+                src={logo}
+                alt='logo'
+                className={`router__logo ${mouseOverOnLogo ? 'router__logo_hover' : ''}`}
+                onMouseOver={() => setMouseOverOnLogo(true)}
+                onMouseLeave={()=> setMouseOverOnLogo(false)}
+              />
+            </Link>
+          </div>
+          <Switch>
+            <Route exact path="/" component={MainPage}/>
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import './index.scss';
 
-import logo from './icons/logo.svg';
 import CreateButton from './components/CreateButton';
 import Board from './components/Board';
 
@@ -11,23 +10,10 @@ import boards from './boards.json';
 
 
 const MainPage = () => {
-  const [mouseOverOnLogo, setMouseOverOnLogo] = useState(false);
   const [boardsList, setBoardsList] = useState(boards);
 
   return (
     <div className='main-page'>
-      <div className='main-page__header'>
-        <Link to={'/'}>
-          <img
-            src={logo}
-            alt='logo'
-            className={`main-page__logo ${mouseOverOnLogo ? 'main-page__logo_hover' : ''}`}
-            onMouseOver={() => setMouseOverOnLogo(true)}
-            onMouseLeave={()=> setMouseOverOnLogo(false)}
-          />
-        </Link>  
-      </div>
-
       <div className='main-page__content'>
         <CreateButton setBoardsList={setBoardsList} boards={boardsList}/>
 
@@ -35,7 +21,9 @@ const MainPage = () => {
           boardsList &&
           boardsList.length &&
           boardsList.map(elem =>
-            <Board title={elem.title} key={elem.id}/>
+            <Link to={`/board/${elem.id}`}>
+              <Board title={elem.title} key={elem.id}/>
+            </Link>
           )
         }
       </div>
