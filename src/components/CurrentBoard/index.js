fe5028c6c15b925b8e70d480ad691012154bb83c
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-02-26 14:58:10 +0300

    Add board title in the Current board page

---
+++ src/components/CurrentBoard/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+import './index.scss';
+
+import boardList from '../MainPage/boards.json';
+
+
+const CurrentBoard = props => {
+  const { boardId } = props.match.params;
+
+  const getBoardTitle = () => {
+    const index = boardList &&
+      boardList.length &&
+      boardList.findIndex(elem => elem.id === +boardId);
+
+    return index >= 0 ? boardList[index] && boardList[index].title : null;
+  }
+
+  return (
+    <div className='current-board'>
+      <div className='current-board__title'>
+        <div>
+          {getBoardTitle()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentBoard;
