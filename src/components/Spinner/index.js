3993e4e0be2948b18185af17124610d549552c8f
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-03-02 11:33:22 +0300

    Create spinner

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+import { Spin } from 'antd';
+
+
+const Spinner = () => {
+  return (
+    <div className='spinner'>
+      <Spin size="large"/>
+    </div>
+  );
+};
+
+export default Spinner;
