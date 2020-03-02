e59e1a5fa8c1286fa795222303874c7b2149dfc9
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-03-02 13:54:04 +0300

    Change tasks consts, reducers and sctions

--- src/store/actions/tasks.js
+++ src/store/actions/tasks.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
+
 import {
   GET_TASKS_SUCCESS,
   GET_TASKS_FAILURE,
   ADD_TASK_SUCCESS,
-  ADD_TASK_FAILURE
+  ADD_TASK_FAILURE,
+  GET_TASKS_BY_LIST_SUCCESS,
+  GET_TASKS_BY_LIST_FAILURE
 } from '../constants/tasks';
 
 
@@ -16,6 +20,18 @@ export const getTasksFailure = error => ({
   payload: error
 });
 
+export const getTasksAsync = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`tasks`);
+      dispatch(getTasksSuccess(data))
+    }
+    catch (error) {
+      dispatch(getTasksFailure(error))
+    }
+  }
+};
+
 export const addTaskSuccess = task => ({
   type: ADD_TASK_SUCCESS,
   payload: task
@@ -24,4 +40,26 @@ export const addTaskSuccess = task => ({
 export const addTaskFailure = error => ({
   type: ADD_TASK_FAILURE,
   payload: error
-})
+});
+
+export const getTasksByListSuccess = tasks => ({
+  type: GET_TASKS_BY_LIST_SUCCESS,
+  payload: tasks
+});
+
+export const getTasksByListFailure = error => ({
+  type: GET_TASKS_BY_LIST_FAILURE,
+  payload: error
+});
+
+export const getTasksByListAsync = listId => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`tasks/${listId}`);
+      dispatch(getTasksByListSuccess(data))
+    }
+    catch (error) {
+      dispatch(getTasksByListFailure(error))
+    }
+  }
+};
--- src/store/reducers/tasks.js
+++ src/store/reducers/tasks.js
@@ -2,12 +2,15 @@ import {
   GET_TASKS_SUCCESS,
   GET_TASKS_FAILURE,
   ADD_TASK_SUCCESS,
-  ADD_TASK_FAILURE
+  ADD_TASK_FAILURE,
+  GET_TASKS_BY_LIST_SUCCESS,
+  GET_TASKS_BY_LIST_FAILURE
 } from '../constants/tasks';
 
 
 const initialState = {
   allTasks: [],
+  tasksByList: [],
   error: ''
 }
 
@@ -23,6 +26,12 @@ export default function tasks(state = initialState, action) {
       return { ...state, allTasks: [...state.allTasks, action.payload] };
 
     case ADD_TASK_FAILURE: 
+      return { ...state, error: action.payload };
+
+    case GET_TASKS_BY_LIST_SUCCESS:
+      return { ...state, tasksByList: action.payload };
+
+    case GET_TASKS_BY_LIST_FAILURE: 
       return { ...state, error: action.payload };
       
     default:
