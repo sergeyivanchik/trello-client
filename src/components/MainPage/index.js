cb1020ab9cd3f45752f4c0ba65127105d055c12e
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-03-02 11:53:11 +0300

    Add spinner in the Main page

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -6,34 +6,45 @@ import './index.scss';
 
 import CreateButton from './components/CreateButton';
 import Board from './components/Board';
+import Spinner from '../Spinner';
 
 import { getBoardsAsync } from '../../store/actions/boards';
+import { showSpinner, hideSpinner } from '../../store/actions/spinner';
 
 
 const MainPage = () => {
   const allBoards = useSelector(state => state.boards.allBoards);
+  const isLoading = useSelector(state => state.spinner.isLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(showSpinner());
     dispatch(getBoardsAsync());
+    dispatch(hideSpinner());
   }, []);
 
   return (
-    <div className='main-page'>
-      <div className='main-page__content'>
-        <CreateButton boards={allBoards}/>
+    <>
+    {
+      isLoading
+        ? <Spinner/>
+        : <div className='main-page'>
+            <div className='main-page__content'>
+              <CreateButton boards={allBoards}/>
 
-        {
-          allBoards &&
-          !!allBoards.length &&
-          allBoards.map(elem =>
-            <Link to={`/board/${elem.id}`} key={elem.id}>
-              <Board title={elem.title} key={elem.id}/>
-            </Link>
-          )
-        }
-      </div>
-    </div>
+              {
+                allBoards &&
+                !!allBoards.length &&
+                allBoards.map(elem =>
+                  <Link to={`/board/${elem.id}`} key={elem.id}>
+                    <Board title={elem.title} key={elem.id}/>
+                  </Link>
+                )
+              }
+            </div>
+          </div>
+    }
+    </>
   );
 };
 
