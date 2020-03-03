2a36dba45a90187c6333c1505721803562b493ee
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-03-03 13:12:25 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import React, { Fragment, useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.scss'
 
-import logo from './components/MainPage/icons/logo.svg';
 import MainPage from '../src/components/MainPage';
 import CurrentBoard from '../src/components/CurrentBoard';
+import Header from '../src/components/Header';
 
 
 const App = () => {
-  const [mouseOverOnLogo, setMouseOverOnLogo] = useState(false);
-
   return (
     <Fragment>
       <Router>
         <div className="router">
-          <div className="router__header">
-            <Link to={'/'}>
-              <img
-                src={logo}
-                alt='logo'
-                className={`router__logo ${mouseOverOnLogo ? 'router__logo_hover' : ''}`}
-                onMouseOver={() => setMouseOverOnLogo(true)}
-                onMouseLeave={()=> setMouseOverOnLogo(false)}
-              />
-            </Link>
-          </div>
+          <Header/>
           <Switch>
             <Route exact path="/" component={MainPage}/>
             <Route exact path="/board/:boardId" component={CurrentBoard}/>
