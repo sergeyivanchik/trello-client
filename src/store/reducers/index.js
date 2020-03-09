16bf7b9a0c2bc48f73534711894410a9f1b64e9c
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-03-09 12:41:46 +0300

    Create user store

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,12 +3,14 @@ import boards from './boards';
 import lists from './lists';
 import tasks from './tasks';
 import spinner from './spinner';
+import users from './users';
 
 const allReducers = combineReducers ({
   boards,
   lists,
   tasks,
-  spinner
+  spinner,
+  users
 });
 
 export default allReducers;
