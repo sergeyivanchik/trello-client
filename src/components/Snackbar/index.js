622f51e29a553c5168735a82805ea7e7e823c5f6
Sergey Ivanchik <siarhei.ivanchyk@codex-soft.com>
2020-03-11 10:59:36 +0300

    Change Snackbar

--- src/components/Snackbar/index.js
+++ src/components/Snackbar/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useDispatch } from  'react-redux';
 
 import { notification } from 'antd';
 
+import { hideSnackbar } from '../../store/actions/snackbar';
+
 
 const Snackbar = ({ type, message, description, isShow }) => {
+  const dispatch = useDispatch();
+
   const openNotificationWithIcon = () => {
     notification[type]({
       message,
       description,
-      placement: 'bottomLeft'
+      placement: 'bottomLeft',
+      onClose: () => dispatch(hideSnackbar())
     });
   };
 
